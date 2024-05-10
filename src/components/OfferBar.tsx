0f2f27645e83d4acab7455313fe496ed2034f787
Suraj Pradhan <71517930+celestialsuraj@users.noreply.github.com>
2024-05-10 22:20:26 +0530

    add offerbar

---
+++ src/components/OfferBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OfferBar = () => {
+  return (
+    <div className="flex items-center justify-center h-6 py-5 bg-blue font text-sm">
+      30% off storewide - Limited time!
+    </div>
+  );
+};
+
+export default OfferBar;
