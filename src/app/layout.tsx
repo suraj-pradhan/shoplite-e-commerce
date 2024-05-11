37a7a0121efde66093c9f4cbe44f26858b9b1ca8
Suraj Pradhan <71517930+celestialsuraj@users.noreply.github.com>
2024-05-11 13:41:07 +0530

    minor change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shoplite | Furniture Store",
+  title: "Shoplite | Gift & Decoration Store",
   description: "E-Commerce Store",
 };
 
