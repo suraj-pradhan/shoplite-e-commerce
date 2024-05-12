6523ab4e145c884d3d1faf8830b7e707feeae589
Suraj Pradhan <71517930+celestialsuraj@users.noreply.github.com>
2024-05-12 21:10:17 +0530

    add cross icon to offerbar

--- src/components/OfferBar.tsx
+++ src/components/OfferBar.tsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { RightArrow, TicketPercent } from "./Icons";
+import { CrossIcon, RightArrow, TicketPercent } from "./Icons";
 import Link from "next/link";
 
 const OfferBar = () => {
   return (
-    <div className="flex items-center justify-center h-8 py-5 bg-light-blue text-[13px] xs:text-[12.4px] ">
-      <TicketPercent /> &nbsp; 30% off storewide - Limited time! &nbsp;
-      <div className=" text-blue relative ">
-        <Link className="flex" href="/">
-          Shop Now&nbsp;
-          <RightArrow />
-          <span className="absolute top-[19px] xs:top-[18px] left-0 border w-[6.4em]"></span>
-        </Link>
+    <div className="flex items-center justify-center h-8 py-5 bg-light-blue relative text-[13px] xs:text-[12.4px] xs:justify-start xs:px-5 ">
+      <div className="flex items-center justify-start">
+        <TicketPercent /> &nbsp; 30% off storewide - Limited time! &nbsp;
+        <div className=" text-blue relative ">
+          <Link className="flex" href="/">
+            Shop Now&nbsp;
+            <RightArrow />
+            <span className="absolute top-[19px] xs:top-[18px] left-0 border w-[6.4em]"></span>
+          </Link>
+        </div>
+      </div>
+      <div className="absolute right-6 xs:right-4">
+        <CrossIcon />
       </div>
     </div>
   );
