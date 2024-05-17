e4a5b6fa55606c19789766e21c0a9b898913b767
Suraj Pradhan <71517930+celestialsuraj@users.noreply.github.com>
2024-05-17 20:37:07 +0530

    minor improvements

--- src/components/OfferBar.tsx
+++ src/components/OfferBar.tsx
@@ -1,28 +1,38 @@
+"use client";
 import React from "react";
 import { CrossIcon, RightArrow, TicketPercent } from "./Icons";
 import Link from "next/link";
 
-const OfferBar = () => {
+export default function OfferBar() {
+  const [status, setStatus] = React.useState(true);
+
   return (
-    <div
-      className="flex items-center justify-center h-8 py-5 bg-light-blue relative
-     text-[13px] xs:text-[12.4px] xs:justify-start xs:px-5"
-    >
-      <div className="flex items-center justify-start">
-        <TicketPercent /> &nbsp; 30% off storewide - Limited time! &nbsp;
-        <div className=" text-blue relative ">
-          <Link className="flex" href="/">
-            Shop Now&nbsp;
-            <RightArrow />
-            <span className="absolute top-[19px] xs:top-[18px] left-0 border w-[6.4em]"></span>
-          </Link>
+    <div>
+      {status ? (
+        <div
+          className="flex items-center justify-center h-8 py-5 bg-light-blue relative
+                      text-[13px] xs:text-[12.4px] xs:justify-start xs:px-5"
+        >
+          <div className="flex items-center justify-start">
+            <TicketPercent /> &nbsp; 30% off storewide - Limited time! &nbsp;
+            <div className=" text-blue relative ">
+              <Link className="flex" href="/">
+                Shop Now&nbsp;
+                <RightArrow />
+                <span className="absolute top-[19px] xs:top-[18px] left-0 border-t w-[6.4em]"></span>
+              </Link>
+            </div>
+          </div>
+          <button onClick={() => setStatus(false)}>
+            <span
+              className="absolute top-[14px]
+         right-6 xs:right-4"
+            >
+              <CrossIcon />
+            </span>
+          </button>
         </div>
-      </div>
-      <div className="absolute right-6 xs:right-4">
-        <CrossIcon />
-      </div>
+      ) : null}
     </div>
   );
-};
-
-export default OfferBar;
+}
