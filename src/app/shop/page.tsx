e7dd4933fcf0a674155859da348e188cca4dc7e4
Suraj Pradhan <71517930+celestialsuraj@users.noreply.github.com>
2024-05-18 16:09:15 +0530

    add shop page

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import OfferBar from "@/components/OfferBar";
+
+export default function Shop() {
+  return (
+    <main>
+      {/* <OfferBar /> */}
+      <NavBar />
+      <Footer />
+    </main>
+  );
+}
