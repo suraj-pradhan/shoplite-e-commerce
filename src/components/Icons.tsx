ce7b405eff5e466bb2fe090dd05ba2e3d1a769da
Suraj Pradhan <71517930+celestialsuraj@users.noreply.github.com>
2024-05-18 17:56:25 +0530

    add hamburger icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -276,3 +276,20 @@ export const CrossIcon = () => (
     />
   </svg>
 );
+
+export const HamburgerIcon = () => (
+  <svg
+    width="12"
+    height="10"
+    viewBox="0 0 12 10"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1 1H11M1 5H11M1 9H11"
+      stroke="#141718"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+    />
+  </svg>
+);
